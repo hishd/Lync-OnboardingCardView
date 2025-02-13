9ae355cbf5ce1668a92ace7d84d14485347eb884
Hishara Dilshan <hisharadilshan3@gmail.com>
2025-02-13 04:48:22 +0000

    Initial Commit

---
+++ OnboardingCardView/OnboardingCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingCardViewApp.swift
+//  OnboardingCardView
+//
+//  Created by Hishara Dilshan on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
